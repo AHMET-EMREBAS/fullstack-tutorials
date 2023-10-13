dc5a600661c2e923acb1829ce7d79752b9fddefe
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-10-12 20:52:44 -0500

    done

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { catchError, of, repeat, tap } from 'rxjs';
+import { catchError, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'techbir-root',
@@ -16,7 +16,7 @@ export class AppComponent {
     this.httpClient
       .get('api')
       .pipe(
-        repeat({ count: 3, delay: 4000 }),
+        take(1),
         catchError((err, caught) => {
           return of(err);
         }),
