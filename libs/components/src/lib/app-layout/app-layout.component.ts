50839f2dd3c130c30676977836b4b0605aec238a
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-10-31 12:34:32 -0500

    fonts

--- libs/components/src/lib/app-layout/app-layout.component.ts
+++ libs/components/src/lib/app-layout/app-layout.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'techbir-app-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss'],
 })
